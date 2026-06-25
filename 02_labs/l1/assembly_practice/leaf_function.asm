ccd54acc2edaf98765576530c81a75e0e7dc41de
WhisperingRock <xpropellorheadx@gmail.com>
2026-06-24 19:43:36 -0700

    more practice for l1

--- 02_labs/l1/assembly_practice/leaf_function.asm
+++ 02_labs/l1/assembly_practice/leaf_function.asm
@@ -1,8 +1,9 @@
-# Purpose : perform y = a * b
-# Assumption : a >= 0, b >= 0
+# Purpose : perform y = (a+b)-(c+d)
 # Inputs : [int] a = a0,
 #		 b = a1,
-# Return : [int] y = s1
+#		 c = a2,
+#		 d = a3
+# Return : [int] y = s3
 
 # note : preserved/saved registers are s0-s11, sp, and ra
 # note : nonpreserved are a0-a7, t0-t6, 
@@ -13,49 +14,46 @@
 main:
 
 # ~~~~ init vars ~~~~
-	addi a0, zero, 0	
-	addi a1, zero, 123	
+	addi a0, zero, -5	
+	addi a1, zero, 10	
+	addi a2, zero, -8	
+	addi a3, zero, 2	
 	
 # ~~~~ caller-saves nonpreserved ~~~~
 	# ~~ 1. Make space on stack for 4 registers ~~
-	addi sp, sp, -8
+	addi sp, sp, -16
 	
 	# ~~ 2. Store reg values in memory ~~
-	sw a0, 4(sp)
-	sw a1, 0(sp)
-
+	sw a0, 12(sp)	# save in next spot
+	sw a1, 8(sp)
+	sw a2, 4(sp)
+	sw a3, 0(sp)
+	
 	# ~~ 3. Execute function
-	jal multiply
+	jal diffofsum
 	add s3, a0, zero	# a0 is the return val register
 	
 	# ~~ 4. Restore og values from stack
-	lw a0, 4(sp)
-	lw a1, 0(sp)
+	lw a0, 12(sp)
+	lw a1, 8(sp)
+	lw a2, 4(sp)
+	lw a3, 0(sp)
 	
 	# ~~ 5. Deallocate space on stack
-	addi sp, sp, 8
+	addi sp, sp, 16
 
 	# ~~ exit
-	j end
+	jal end
 
-multiply: 
+diffofsum: 
 # we would attempt to perform calle-saved steps (like above)
 # but we do not need to use saved registers s0-s11 here, just
 # scratch registers t0-t6 and a0
 
-	# ~~ init vars ~~
-	addi t0, zero, 0	# b goes to 0
-	addi t1, a0, 0		# t1 = a (const)
-	addi a0, zero, 0	# let a0 = rolling product (easy return)
-				# let a1 = b
-				
-while:	beq a1, t0 endwhile	#while b > 0
-	add a0, a0, t1		# t1 += a
-	addi a1, a1, -1		# b--
-	j while
+	add a0, a0, a1
+	add a2, a2, a3
+	sub a0, a0, a2
 	
-	
-endwhile:
 	# ~~ return with return register (+4) ~~
 	jr ra
 
